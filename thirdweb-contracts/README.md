95aed26be74518381b337b7d828b0173678d6a72
waleedsadeh <waleedsadeh5@gmail.com>
2025-03-26 17:51:12 +0000

    add functions like create, fund, get donators funded the campagin and get campaign we have

--- thirdweb-contracts/README.md
+++ thirdweb-contracts/README.md
@@ -3,7 +3,7 @@
 Create a project using this example:
 
 ```bash
-npx thirdweb create --contract --template hardhat-javascript-starter
+npx thirdweb@latest create --contract
 ```
 
 You can start editing the page by modifying `contracts/Contract.sol`.
---
+++ thirdweb-contracts/contracts/CrowdFundingContract.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFundingContract {
+    struct Campaign {
+        string title;
+        string description;
+        uint256 goal;
+        uint256 moneyRaised;
+        uint256 deadline;
+        address owner;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public campaignsCount = 0;
+
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        address _owner,
+        uint256 _goal,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[campaignsCount];
+        require(campaign.deadline < block.timestamp, "Deadline must be set in the future");
+
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.owner = _owner;
+        campaign.goal = _goal;
+        campaign.moneyRaised = 0;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+
+        campaignsCount++;
+        return campaignsCount - 1;
+    }
+
+    function fundCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+         
+        if(sent) {
+            campaign.moneyRaised += amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+
+    }
+
+    function getCampaigns() view public returns (Campaign[] memory){
+        Campaign[] memory allCampaigns = new Campaign[](campaignsCount);
+
+        for (uint256 c = 0 ; c < campaignsCount; c++) {
+            Campaign storage item = campaigns[c]; 
+            allCampaigns[c] = item;
+        }
+        return allCampaigns;
+    }
+}
