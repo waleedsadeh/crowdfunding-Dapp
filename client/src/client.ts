8e98e5b625054740bb6a39d51b6f223da4e27300
waleedsadeh <waleedsadeh5@gmail.com>
2025-03-27 18:34:53 +0000

    made the client folder using npx thirdweb create app and installed react-router-dom and got the client id from thirdweb

--- client/src/client.ts
+++ client/src/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const clientId = "61d941252b3c2ee1b017e5ce82faab7a";
 
 export const client = createThirdwebClient({
   clientId: clientId,
