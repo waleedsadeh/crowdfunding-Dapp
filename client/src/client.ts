241d11318d1a38ba86cc5ce4a10a69f95dbba0b6
waleedsadeh <waleedsadeh5@gmail.com>
2025-03-27 19:13:21 +0000

    after adding the client folder with thirdweb framework, i deleted the default src file to start from scratch

--- client/src/client.ts
+++
@@ -1,9 +0,0 @@
-import { createThirdwebClient } from "thirdweb";
-
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = "61d941252b3c2ee1b017e5ce82faab7a";
-
-export const client = createThirdwebClient({
-  clientId: clientId,
-});
