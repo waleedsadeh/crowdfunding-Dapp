9e6c59ba8ffe16004666d9599f8a782e579b90b5
waleedsadeh <waleedsadeh5@gmail.com>
2025-03-29 22:58:49 +0000

    the campagin form is working and tested it and next is to add the contract functions

--- thirdweb-app/src/pages/CreateCampaign.jsx
+++ thirdweb-app/src/pages/CreateCampaign.jsx
@@ -7,8 +7,9 @@ import { checkIfImage } from "../utils/util";
 function CreateCampaign() {    
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
     }
     const [form, setForm] = useState({
         name: "",
@@ -19,6 +20,10 @@ function CreateCampaign() {
         image: "",
     });
 
+    const handleFormFieldChange = (fieldName, e) => {
+        setForm({ ...form, [fieldName]: e.target.value });
+    }
+
     return (
         <div className="bg-[#171a19] flex justify-center items-center flex-col rounded-[20px] sm:p-10 p-4">
             {isLoading && 'Loading...'}
@@ -27,15 +32,15 @@ function CreateCampaign() {
             </div>
             <form onSubmit={handleSubmit} className="mt-[64px] w-full flex flex-col gap-[30px]">
                 <div className="flex flex-wrap gap-[40px]">
-                    <FormField labelName="Your Name *" placeholder="Waleed Sadeh" inputType="text" value={form.name} handleChange={() => {}}/>
-                    <FormField labelName="Campaign Title *" placeholder="Need new PC" inputType="text" value={form.title} handleChange={() => {}}/>
+                    <FormField labelName="Your Name *" placeholder="Waleed Sadeh" inputType="text" value={form.name} handleChange={(e) => {handleFormFieldChange("name", e)}}/>
+                    <FormField labelName="Campaign Title *" placeholder="Need new PC" inputType="text" value={form.title} handleChange={(e) => {handleFormFieldChange("title", e)}}/>
                 </div>
-                <FormField labelName="description *" placeholder="Write about the campaign" isTextArea={true} value={form.description} handleChange={() => {}}/>
+                <FormField labelName="description *" placeholder="Write about the campaign" isTextArea={true} value={form.description} handleChange={(e) => {handleFormFieldChange("description", e)}}/>
                 <div className="flex flex-wrap gap-[40px]">
-                    <FormField labelName="Amount *" placeholder="ETH 5.10" inputType="text" value={form.target} handleChange={() => {}}/>
+                    <FormField labelName="Amount *" placeholder="ETH 5.10" inputType="text" value={form.target} handleChange={(e) => {handleFormFieldChange("target", e)}}/>
                     <FormField labelName="End Date *" placeholder="Select a date" inputType="date" value={form.deadline } handleChange={(date) => setForm({ ...form, deadline: date })}/>
                 </div>
-                <FormField labelName="Campaign image *" placeholder="Place image URL of your campaign" inputType="url" value={form.image} handleChange={() => {}}/>
+                <FormField labelName="Campaign image *" placeholder="Place image URL of your campaign" inputType="url" value={form.image} handleChange={(e) => {handleFormFieldChange("image", e)}}/>
                 <div className="flex justify-center items-center mt-[40px]">
                     <CustomButton btnType="submit" title="Submit new campaign" styles="bg-[#1dc071]"/>
                 </div>
