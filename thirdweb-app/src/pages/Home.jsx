b0bf2b8a37e55d9c8e99f0f6ca2ffeb7a2148443
waleedsadeh <waleedsadeh5@gmail.com>
2025-04-05 20:51:26 +0100

    added a filter in home pages to show the campaigns with no expiry date

--- thirdweb-app/src/pages/Home.jsx
+++ thirdweb-app/src/pages/Home.jsx
@@ -1,21 +1,31 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStateContext } from '../contract-functions';
 import { DisplayCampaigns } from '../components';
 
 function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
-  const { address, contract, getCampaigns} = useStateContext();
+  const { address, contract, getCampaigns } = useStateContext();
+
   const fetchCampaigns = async () => {
     setIsLoading(true);
     const data = await getCampaigns();
-    setCampaigns(data);
+
+    // Filter campaigns where days left is greater than 0
+    const filteredCampaigns = data.filter((campaign) => {
+      const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+      const daysLeft = Math.floor((campaign.deadline - currentTime) / (24 * 60 * 60));
+      return daysLeft >= 0;
+    });
+
+    setCampaigns(filteredCampaigns);
     setIsLoading(false);
-  }
+  };
+
   useEffect(() => {
-    if(contract) fetchCampaigns();
+    if (contract) fetchCampaigns();
   }, [address, contract]);
-  
+
   return (
     <DisplayCampaigns
       title="All Campaigns"
